b73869bef7961e37aef58d98a71f60b7fa3c9bde
Vidyasagar Kumar <vksagar82@gmail.com>
2023-08-18 22:43:58 +0530

    Update the routes in app.js file and fix the imports

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { ThemeProvider, createTheme } from "@mui/material";
 import { AuthContextProvider } from "../src/common/Auth/AuthContext";
+import ProductDetail from "../src/components/ProductDetails/ProductDetail";
 import ProductsContainer from "../src/components/Products/Products";
-import LogIn from "./components/Login/LogIn";
-import SignUp from "./components/SignUp/SignUp";
+import LogIn from "../src/components/LogIn/LogIn";
+import SignUp from "../src/components/SignUp/SignUp";
+import AddEditProduct from "../src/components/addProduct/AddEditProduct";
+import Orders from "../src/components/Orders/Orders";
 
 const appTheme = createTheme({
   palette: {
@@ -23,11 +26,14 @@ function App() {
       <ThemeProvider theme={appTheme}>
         <BrowserRouter>
           <Routes>
+            <Route path="/edit-product/:id" element={<AddEditProduct />} />
+            <Route path="/add-product" element={<AddEditProduct />} />
             <Route path="/login" element={<LogIn />} />
-            <Route exact path="/" element={<Navigate to="/LogIn" />} />
             <Route path="/products/:id" element={<ProductDetail />} />
             <Route exact path="/products" element={<ProductsContainer />} />
+            <Route exact path="/" element={<Navigate to="/login" />} />
             <Route path="/signup" element={<SignUp />} />
+            <Route path="/order" element={<Orders />} />
           </Routes>
         </BrowserRouter>
       </ThemeProvider>
